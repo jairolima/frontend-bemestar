aba7f15740a4a951e635c39586b6f7e45864117b
Jairo Lima <msgjairo@gmail.com>
2020-08-03 16:57:39 -0300

    width adm

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 // producao ajuste link
 const api = axios.create({
-  baseURL: 'https://api.policlinicabemestar.com',
-  // baseURL: 'http://localhost:3333',
+  // baseURL: 'https://api.policlinicabemestar.com',
+  baseURL: 'http://localhost:3333',
 });
 
 export default api;
