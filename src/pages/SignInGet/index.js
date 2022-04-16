a7860b58fbf6b665e4d904f09bd0581c7c0f71da
Jairo Lima <msgjairo@gmail.com>
2022-04-16 17:36:10 -0300

    up

--- src/pages/SignInGet/index.js
+++ src/pages/SignInGet/index.js
@@ -1,11 +1,14 @@
 import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
+import { useParams } from 'react-router-dom';
 import { Container } from './styles';
 import { signInRequest } from '~/store/modules/auth/actions';
 import history from '~/services/history';
 
-export default function SignInGet(phone, password_hash) {
+export default function SignInGet() {
   const dispatch = useDispatch();
+
+  const { phone, password_hash } = useParams();
 
   useEffect(() => {
     dispatch(signInRequest(phone, password_hash));
