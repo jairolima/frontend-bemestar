eb4de10c9e4351499c4645dde23962f81f8fa7c6
Jairo Lima <36547876+jairolima@users.noreply.github.com>
2024-04-16 12:19:52 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 // producao ajuste link
 const api = axios.create({
-  baseURL: 'https://api.policlinicabemestar.com',
+  baseURL: 'https://bemestar-legado-nodejs.y2n3tv.easypanel.host/',
   // baseURL: 'http://localhost:3333',
 });
 
